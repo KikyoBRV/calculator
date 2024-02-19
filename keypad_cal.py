6fe4851b2ea812ce5c54c28174ae0e234b300346
Buravit Yenjit <khiaokrub@gmail.com>
2024-02-19 21:42:21 +0700

    7th commit: change keypad_cal.py

--- keypad_cal.py
+++ keypad_cal.py
@@ -1,29 +1,9 @@
-from keypad_ui import KeypadUI
 from math import *
 
 
-class CalculatorController:
+class KeypadCal:
     def __init__(self):
-        # Initialize any variables or state needed for the calculator
-        self.result = 0
+        pass
 
-    def add(self, num1, num2):
-        # Method to add two numbers
-        return num1 + num2
-
-    def subtract(self, num1, num2):
-        # Method to subtract num2 from num1
-        return num1 - num2
-
-    def multiply(self, num1, num2):
-        # Method to multiply two numbers
-        return num1 * num2
-
-    def divide(self, num1, num2):
-        # Method to divide num1 by num2
-        if num2 != 0:
-            return num1 / num2
-        else:
-            return "Error: Division by zero"
-
-    def exp(self, num1, num2):
+    def equal(self):
+        pass
